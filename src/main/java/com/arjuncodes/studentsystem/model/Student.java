e134a3cbc474c4eb5c9334f249bc62979fc3c6b9
arjungautam1 <arjungautam5431@gmail.com>
2021-07-06 18:55:24 +0545

    connection dabase success

--- src/main/java/com/arjuncodes/studentsystem/model/Student.java
+++ src/main/java/com/arjuncodes/studentsystem/model/Student.java
@@ -1,4 +1,42 @@
 package com.arjuncodes.studentsystem.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String address;
+
+    public Student() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
--- src/main/java/com/arjuncodes/studentsystem/repository/StudentRepository.java
+++ src/main/java/com/arjuncodes/studentsystem/repository/StudentRepository.java
@@ -1,4 +1,9 @@
 package com.arjuncodes.studentsystem.repository;
 
-public interface StudentRepository {
+import com.arjuncodes.studentsystem.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Integer> {
 }
